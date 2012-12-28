a6e67755834391d6c42453dbfe76ec93a3d6169a
Georges <georges@new-host-3.home>
2012-12-28 16:27:34 +0100

    update db9

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,7 @@ module ECommerce
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
     config.assets.precompile += %w( files )
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
