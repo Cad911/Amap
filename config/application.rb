3a491c2b0a75bdb4d95ee26524b65ceaec5b44a0
Georges <georges@new-host-3.home>
2012-12-28 15:23:35 +0100

    update db5

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,7 @@ module ECommerce
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( files )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
