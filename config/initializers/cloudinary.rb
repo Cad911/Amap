a2358d5de97c000387d71f783e7bd179a00576e2
Georges <georges@new-host.home>
2013-03-25 15:07:49 +0100

    cloudinary6

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'ht5glykmi'
-  config.api_key = '511354594823599'
-  config.api_secret = 'Uq7RlsjOl6QbXsIc89LPLbnFyOc'
-  # config.cdn_subdomain = true
-end
